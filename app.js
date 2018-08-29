2e55c9a826a58f5951fe6a3ea41d50af1bb0d78a
wongprom <jimmybackstrom@hotmail.com>
2018-08-29 14:16:50 +0200

    Get element from html to work with from app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// Game values
+let min = 1,
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
+
+//Get elements from Html
+let ELgame = document.querySelector('#game'),
+  ELminNum = document.querySelector('.min-num'),
+  ELmaxNum = document.querySelector('.max-num'),
+  ELguessBtn = document.querySelector('#guess-btn'),
+  ELguessInput = document.querySelector('#guess-input'),
+  ELmessage = document.querySelector('.message');
